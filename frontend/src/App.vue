6d5dfce1651fa3e48257d790c6692cb2cd5b0010
Pawel Krystian <187740@edu.p.lodz.pl>
2022-03-24 20:57:08 +0100

    feat: FE init with hello world app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,26 @@
-<template lang="html">
-  <div>
-    <router-view/>
-  </div>
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
